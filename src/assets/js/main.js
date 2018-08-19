5c549878005c83f050ca5251be9604098d93017e
Lior Rabinovich <lior.rabinovich@gthsoftware.com>
2018-08-19 16:53:22 +0300

    Added sound to example

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,35 @@
-class bla {
+class Main {
     constructor() {
-        let bla = 'test';
-        alert(bla);
+        this.ready(() => {
+            this.buttonSound = new Audio("assets/sounds/button.mp3");
+            this.$listItems = document.getElementsByTagName('li');
+
+            this.addEventsListener();
+
+            let bla = 'Alert test';
+            alert(bla);
+        });
     }
+
+    ready(fn) {
+        if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
+            fn();
+        } else {
+            document.addEventListener('DOMContentLoaded', fn);
+        }
+    }
+
+    addEventsListener() {
+        let self = this;
+        Array.from(this.$listItems).forEach(function ($element) {
+            $element.addEventListener('click', function () {
+                self.buttonSound.pause();
+                self.buttonSound.currentTime = 0;
+                self.buttonSound.play();
+            });
+        });
+    }
+
 }
 
-new bla();
+new Main();
